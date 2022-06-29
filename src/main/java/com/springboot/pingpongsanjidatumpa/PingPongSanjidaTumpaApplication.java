09c4685292eb771a2fb95e26a38c3a86b55b84ec
Sanjida1726 <tumpa.sanjida@gmail.com>
2022-06-29 15:47:44 +0100

    Initial Commit

---
+++ src/main/java/com/springboot/pingpongsanjidatumpa/PingPongSanjidaTumpaApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.pingpongsanjidatumpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPongSanjidaTumpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingPongSanjidaTumpaApplication.class, args);
+        System.out.println("Welcome to my Spring project");
+    }
+
+}
---
+++ src/test/java/com/springboot/pingpongsanjidatumpa/PingPongSanjidaTumpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.pingpongsanjidatumpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPongSanjidaTumpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
